3c704217cae446f20bccda165f08ddd0ccb5c95e
Maurice Laveaux <m.laveaux@tue.nl>
2026-02-15 13:40:54 +0100

    Extended the verification script.

--- scripts/verify.py
+++ scripts/verify.py
@@ -4,6 +4,7 @@ import logging
 import shutil
 import re
 import subprocess
+import json
 
 from library import MyLogger, run_program
 from prepare import EXPERIMENTS
@@ -14,6 +15,8 @@ mapping_regex = re.compile(r"(.*)=(.*)")
 # A regex matching a transition in the aut format '(from, action, to)'
 transition_regex = re.compile(r"\(([0-9]*), \"(.*)\", ([0-9]*)\)")
 
+# Extract the product (zeroes and ones) from the pbes file: minepump_fts_projected.renamed_0000001000.phi1.mcf.pbes
+product_regex = re.compile(r".*_projected\.renamed_([01]+)\..*\.pbes")
 
 def main():
     """Verifies the results of the variability solvers."""
@@ -59,84 +62,122 @@ def main():
             #             logger
             #         )
 
-            if ".renamed.aut" in path:
-                projected_aut = path.replace(".renamed.aut", "_projected.aut")
-                run_program(
-                    [
-                        merc_vpg,
-                        "project",
-                        path,
-                        os.path.join(directory, "FD"),
-                        projected_aut,
-                    ],
-                    logger,
-                )
+            # if ".renamed.aut" in path:
+            #     projected_aut = path.replace(".renamed.aut", "_projected.aut")
+            #     run_program(
+            #         [
+            #             merc_vpg,
+            #             "project",
+            #             path,
+            #             os.path.join(directory, "FD"),
+            #             projected_aut,
+            #         ],
+            #         logger,
+            #     )
 
-        for file in os.listdir(tmp_directory):
-            path = tmp_directory + file
+        # for file in os.listdir(tmp_directory):
+        #     path = tmp_directory + file
 
-            if "_projected" in path and "renamed" not in path:
-                aut_renamed_file = path.replace(
-                    "_projected", "_projected.renamed"
-                )
+        #     if "_projected" in path and "renamed" not in path:
+        #         aut_renamed_file = path.replace(
+        #             "_projected", "_projected.renamed"
+        #         )
 
-                # Rename the action labels in the aut file based on the mapping computed above
-                with open(aut_renamed_file, "w", encoding="utf-8") as outfile:
-                    with open(path, encoding="utf-8") as file:
-                        for line in file.readlines():
-                            result = transition_regex.match(line)
-                            if result is not None:
-                                action = result.group(2)
-                                # Remove the action parameters between brackets
-                                action = re.sub(r"\(.*\)", "", action)
-                                outfile.write(
-                                    f'({result.group(1)},"{action}",{result.group(3)})\n'
-                                )
-                            else:
-                                outfile.write(line)
+        #         # Rename the action labels in the aut file based on the mapping computed above
+        #         with open(aut_renamed_file, "w", encoding="utf-8") as outfile:
+        #             with open(path, encoding="utf-8") as file:
+        #                 for line in file.readlines():
+        #                     result = transition_regex.match(line)
+        #                     if result is not None:
+        #                         action = result.group(2)
+        #                         # Remove the action parameters between brackets
+        #                         action = re.sub(r"\(.*\)", "", action)
+        #                         outfile.write(
+        #                             f'({result.group(1)},"{action}",{result.group(3)})\n'
+        #                         )
+        #                     else:
+        #                         outfile.write(line)
+
+    #
 
     for experiment in EXPERIMENTS:
         directory, mcrl2_name, properties = experiment
 
         tmp_directory = directory + "tmp/"
 
-        # Generate pbes for every property
-        for prop in properties:
-            for file in os.listdir(tmp_directory):
-                path = tmp_directory + file
+    #     # Generate pbes for every property
+    #     for prop in properties:
+    #         for file in os.listdir(tmp_directory):
+    #             path = tmp_directory + file
 
-                mcrl2_name = os.path.join(directory, mcrl2_name)
-                pbes_file = path.replace(".aut", f".{prop}.pbes")
+    #             mcrl2_name = os.path.join(directory, mcrl2_name)
+    #             pbes_file = path.replace(".aut", f".{prop}.pbes")
 
-                if "_projected.renamed" in path and ".aut" in path:
-                    run_program(
-                        [
-                            lts2pbes,
-                            "-f",
-                            os.path.join(directory, prop),
-                            "-m",
-                            mcrl2_name,
-                            path,
-                            pbes_file,
-                        ],
-                        logger,
-                    )
+    #             if "_projected.renamed" in path and ".aut" in path:
+    #                 print("Generating pbes for " + path + " and " + prop)
+    #                 run_program(
+    #                     [
+    #                         lts2pbes,
+    #                         "-f",
+    #                         os.path.join(directory, prop),
+    #                         "-m",
+    #                         mcrl2_name,
+    #                         path,
+    #                         pbes_file,
+    #                     ],
+    #                     logger,
+    #                 )
 
-        for prop in properties:
-            result = {}
+        # for prop in properties:
+        #     result = {"experiment": directory, "property": prop, "solution": {}}
 
-            for pbes in os.listdir(args.output):
-                if "pbes" in pbes and prop in pbes:
-                    print("Found pbes file: " + pbes)
-                    pbes_file = os.path.join(args.output, pbes)
+        #     for pbes in os.listdir(tmp_directory):
+        #         if "pbes" in pbes and prop in pbes:
+        #             print("Found pbes file: " + pbes)
+
+        #             product = None
+        #             match = product_regex.match(pbes)
+        #             if match:
+        #                 product = match.group(1)
+
+        #             pbes_file = os.path.join(tmp_directory, pbes)
                     
-                    result = subprocess.run([pbessolve, pbes_file], stdout=subprocess.PIPE, text=True, check=True)
-                    if "true" in result.stdout:
-                        true_count += 1
-                    elif "false" in result.stdout:
-                        false_count += 1
+        #             proc = subprocess.run([pbessolve, pbes_file], stdout=subprocess.PIPE, text=True, check=True)
+        #             if "true" in proc.stdout:
+        #                 result["solution"][product] = {"0": [0], "1": []}
+        #             elif "false" in proc.stdout:
+        #                 result["solution"][product] = {"0": [], "1": [0]}
 
-            print(f"True: {true_count}, False: {false_count}")
+            
+
+        #     with open(os.path.join(args.output, "solution.json"), "a", encoding="utf-8") as f:
+        #         json.dump(result, f)
+        #         f.write("\n")
+
+    # Open both the results.json and solution.json files and compare the results for each property.
+    results = []
+    with open(os.path.join(args.output, "results.json"), encoding="utf-8") as f:
+        results = [json.loads(line) for line in f]
+
+    with open(os.path.join(args.output, "solution.json"), encoding="utf-8") as f:
+        for line in f:
+            solution = json.loads(line)
+
+            for result in results:
+                name = os.path.splitext(solution["property"])[0]
+
+                if name in result["file"]:
+                    expected = result["solution"]
+                    actual = solution["solution"]
+
+                    for key, value in expected[0].items():
+                        if key not in actual:
+                            continue
+
+                        if value != actual[key]:
+                            logger.error(f"Verification failed for {name}. Mismatch for product {key}. Expected: {value}, Actual: {actual[key]}")
+                            continue
+
 
 if __name__ == "__main__":
     try:
