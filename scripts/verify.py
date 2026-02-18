760a1122d82a36147def987ee76e56dae70de0e8
Maurice Laveaux <m.laveaux@tue.nl>
2026-02-18 20:27:10 +0100

    Fixed the verify script.

--- scripts/verify.py
+++ scripts/verify.py
@@ -18,6 +18,7 @@ transition_regex = re.compile(r"\(([0-9]*), \"(.*)\", ([0-9]*)\)")
 # Extract the product (zeroes and ones) from the pbes file: minepump_fts_projected.renamed_0000001000.phi1.mcf.pbes
 product_regex = re.compile(r".*_projected\.renamed_([01]+)\..*\.pbes")
 
+
 def main():
     """Verifies the results of the variability solvers."""
 
@@ -46,115 +47,25 @@ def main():
         # The directory in which to store all generated files
         tmp_directory = directory + "tmp/"
 
-        for file in os.listdir(tmp_directory):
-            path = tmp_directory + file
-            # if ".svpg" in path:
-            #     for solve_variant in ["family", "family-optimised-left"]:
-            #         run_program(
-            #             [
-            #                 merc_vpg,
-            #                 "solve",
-            #                 "--oxidd-node-capacity=1000000",
-            #                 f"--solve-variant={solve_variant}",
-            #                 "--verify-solution",
-            #                 path,
-            #             ],
-            #             logger
-            #         )
+        verify_family_solver(merc_vpg, logger, tmp_directory)
 
-            # if ".renamed.aut" in path:
-            #     projected_aut = path.replace(".renamed.aut", "_projected.aut")
-            #     run_program(
-            #         [
-            #             merc_vpg,
-            #             "project",
-            #             path,
-            #             os.path.join(directory, "FD"),
-            #             projected_aut,
-            #         ],
-            #         logger,
-            #     )
+        project_fts(merc_vpg, logger, tmp_directory, directory)
 
-        # for file in os.listdir(tmp_directory):
-        #     path = tmp_directory + file
-
-        #     if "_projected" in path and "renamed" not in path:
-        #         aut_renamed_file = path.replace(
-        #             "_projected", "_projected.renamed"
-        #         )
-
-        #         # Rename the action labels in the aut file based on the mapping computed above
-        #         with open(aut_renamed_file, "w", encoding="utf-8") as outfile:
-        #             with open(path, encoding="utf-8") as file:
-        #                 for line in file.readlines():
-        #                     result = transition_regex.match(line)
-        #                     if result is not None:
-        #                         action = result.group(2)
-        #                         # Remove the action parameters between brackets
-        #                         action = re.sub(r"\(.*\)", "", action)
-        #                         outfile.write(
-        #                             f'({result.group(1)},"{action}",{result.group(3)})\n'
-        #                         )
-        #                     else:
-        #                         outfile.write(line)
-
-    #
+        rename_projections(tmp_directory)
 
     for experiment in EXPERIMENTS:
         directory, mcrl2_name, properties = experiment
 
         tmp_directory = directory + "tmp/"
 
-    #     # Generate pbes for every property
-    #     for prop in properties:
-    #         for file in os.listdir(tmp_directory):
-    #             path = tmp_directory + file
+        generate_pbes(lts2pbes, logger, directory, mcrl2_name, properties, tmp_directory)
 
-    #             mcrl2_name = os.path.join(directory, mcrl2_name)
-    #             pbes_file = path.replace(".aut", f".{prop}.pbes")
-
-    #             if "_projected.renamed" in path and ".aut" in path:
-    #                 print("Generating pbes for " + path + " and " + prop)
-    #                 run_program(
-    #                     [
-    #                         lts2pbes,
-    #                         "-f",
-    #                         os.path.join(directory, prop),
-    #                         "-m",
-    #                         mcrl2_name,
-    #                         path,
-    #                         pbes_file,
-    #                     ],
-    #                     logger,
-    #                 )
-
-        # for prop in properties:
-        #     result = {"experiment": directory, "property": prop, "solution": {}}
-
-        #     for pbes in os.listdir(tmp_directory):
-        #         if "pbes" in pbes and prop in pbes:
-        #             print("Found pbes file: " + pbes)
-
-        #             product = None
-        #             match = product_regex.match(pbes)
-        #             if match:
-        #                 product = match.group(1)
-
-        #             pbes_file = os.path.join(tmp_directory, pbes)
-                    
-        #             proc = subprocess.run([pbessolve, pbes_file], stdout=subprocess.PIPE, text=True, check=True)
-        #             if "true" in proc.stdout:
-        #                 result["solution"][product] = {"0": [0], "1": []}
-        #             elif "false" in proc.stdout:
-        #                 result["solution"][product] = {"0": [], "1": [0]}
-
-            
-
-        #     with open(os.path.join(args.output, "solution.json"), "a", encoding="utf-8") as f:
-        #         json.dump(result, f)
-        #         f.write("\n")
+        solve_pbes(args, pbessolve, directory, properties, tmp_directory)
 
     # Open both the results.json and solution.json files and compare the results for each property.
+    check_solution(args, logger)
+
+def check_solution(args, logger):
     results = []
     with open(os.path.join(args.output, "results.json"), encoding="utf-8") as f:
         results = [json.loads(line) for line in f]
@@ -167,17 +78,130 @@ def main():
                 name = os.path.splitext(solution["property"])[0]
 
                 if name in result["file"]:
-                    expected = result["solution"]
-                    actual = solution["solution"]
+                    actual = result["solution"][0]
+                    expected = solution["solution"]
 
-                    for key, value in expected[0].items():
+                    for key, value in expected.items():
                         if key not in actual:
-                            continue
+                            continue               
 
                         if value != actual[key]:
-                            logger.error(f"Verification failed for {name}. Mismatch for product {key}. Expected: {value}, Actual: {actual[key]}")
+                            logger.error(
+                                f"Verification failed for {name}, key: {key}, expected: {value}, actual: {actual[key]}"
+                            )
+                            return
                             continue
 
+def solve_pbes(args, pbessolve, directory, properties, tmp_directory):
+    for prop in properties:
+        result = {"experiment": directory, "property": prop, "solution": {}}
+
+        for pbes in os.listdir(tmp_directory):
+            if "pbes" in pbes and prop in pbes:
+                print("Found pbes file: " + pbes)
+
+                product = None
+                match = product_regex.match(pbes)
+                if match:
+                    product = match.group(1)
+
+                pbes_file = os.path.join(tmp_directory, pbes)
+
+                proc = subprocess.run(
+                        [pbessolve, pbes_file],
+                        stdout=subprocess.PIPE,
+                        text=True,
+                        check=True,
+                    )
+                if "true" in proc.stdout:
+                    result["solution"][product] = {"0": [0], "1": []}
+                elif "false" in proc.stdout:
+                    result["solution"][product] = {"0": [], "1": [0]}
+
+        with open(
+                os.path.join(args.output, "solution.json"), "a", encoding="utf-8"
+            ) as f:
+            json.dump(result, f)
+            f.write("\n")
+
+def generate_pbes(lts2pbes, logger, directory, mcrl2_name, properties, tmp_directory):
+    for prop in properties:
+        for file in os.listdir(tmp_directory):
+            path = tmp_directory + file
+
+            mcrl2_name = os.path.join(directory, mcrl2_name)
+            pbes_file = path.replace(".aut", f".{prop}.pbes")
+
+            if "_projected.renamed" in path and ".aut" in path:
+                print("Generating pbes for " + path + " and " + prop)
+                run_program(
+                        [
+                            lts2pbes,
+                            "-f",
+                            os.path.join(directory, prop),
+                            "-m",
+                            mcrl2_name,
+                            path,
+                            pbes_file,
+                        ],
+                        logger,
+                    )
+
+def rename_projections(tmp_directory):
+    for file in os.listdir(tmp_directory):
+        path = tmp_directory + file
+
+        if "_projected" in path and "renamed" not in path:
+            aut_renamed_file = path.replace("_projected", "_projected.renamed")
+
+                # Rename the action labels in the aut file based on the mapping computed above
+            with open(aut_renamed_file, "w", encoding="utf-8") as outfile:
+                with open(path, encoding="utf-8") as file:
+                    for line in file.readlines():
+                        result = transition_regex.match(line)
+                        if result is not None:
+                            action = result.group(2)
+                                # Remove the action parameters between brackets
+                            action = re.sub(r"\(.*\)", "", action)
+                            outfile.write(
+                                    f'({result.group(1)},"{action}",{result.group(3)})\n'
+                                )
+                        else:
+                            outfile.write(line)
+
+def verify_family_solver(merc_vpg, logger, tmp_directory):
+    for file in os.listdir(tmp_directory):
+        path = tmp_directory + file
+        if ".svpg" in path:
+            for solve_variant in ["family", "family-optimised-left"]:
+                run_program(
+                        [
+                            merc_vpg,
+                            "solve",
+                            "--oxidd-node-capacity=1000000",
+                            f"--solve-variant={solve_variant}",
+                            "--verify-solution",
+                            path,
+                        ],
+                        logger,
+                    )
+       
+def project_fts(merc_vpg, logger, tmp_directory, directory):  
+    for file in os.listdir(tmp_directory):
+        path = tmp_directory + file      
+                
+        if ".renamed.aut" in path:
+            projected_aut = path.replace(".renamed.aut", "_projected.aut")
+            run_program(
+                [
+                    merc_vpg,
+                    "project",
+                    path,
+                    os.path.join(directory, "FD"),
+                    projected_aut,
+                ],
+                logger,
+            )
 
 if __name__ == "__main__":
     try:
